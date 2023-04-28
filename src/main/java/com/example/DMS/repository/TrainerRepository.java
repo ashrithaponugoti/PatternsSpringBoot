8bf1408183261f6bcb64eaf871abbb114c48da41
ashrithaponugoti <112123590+ashrithaponugoti@users.noreply.github.com>
2023-04-27 19:13:48 -0500

    first commit

---
+++ src/main/java/com/example/DMS/repository/TrainerRepository.java
@@ -0,0 +1,12 @@
+package com.example.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.DMS.Models.Trainer;
+/**
+ * 
+ * @author s555222
+ *
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
